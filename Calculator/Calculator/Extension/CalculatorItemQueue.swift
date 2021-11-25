143db634b980f3e657c2126681b4bafb1e17e450
GOSARI <goeunrhdms@gmail.com>
2021-11-25 19:56:28 +0900

    refector: CalculatorItemQueue의 init을 extension으로 분리 #10

---
+++ Calculator/Calculator/Extension/CalculatorItemQueue.swift
@@ -0,0 +1,14 @@
+//
+//  CalculatorItemQueue.swift
+//  Calculator
+//
+//  Created by 고은 on 2021/11/25.
+//
+
+import Foundation
+
+extension CalculatorItemQueue {
+    init(_ enQueueElements: [Element]) {
+        self.enQueueElements = enQueueElements
+    }
+}
